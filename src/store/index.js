2f9aefd253f8c1dc2cbfc5b070606e4cbe8c9b86
Nestor Felipe <necrifede@gmail.com>
2019-04-15 00:06:10 +0200

    fix #6 add redux devtools configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers'
-// import thunk from 'redux-thunk';
 
 const logger = store => next => action => {
   console.log('dispatching: ', action);
@@ -9,6 +8,6 @@ const logger = store => next => action => {
   return result;
 }
 
-const extension = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// export default createStore(reducer, applyMiddleware(thunk))
-export default createStore(reducer, extension, applyMiddleware(logger))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(reducer, composeEnhancers(applyMiddleware(logger)))
