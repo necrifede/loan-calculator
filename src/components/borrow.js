812e3e5f289e48aaf238e5a646084768f5ce75eb
Nestor Felipe <necrifede@gmail.com>
2019-05-06 09:21:14 +0200

    fix #7 read latest state to perform server request

--- src/components/borrow.js
+++ src/components/borrow.js
@@ -15,17 +15,26 @@ import { requestLoanCalcDebounce, cancelRequest } from '../store/epics/calcLoanD
  */
 class Borrow extends Component{
 
+  constructor(props) {
+    super(props)
+    this.updateLoan = this.updateLoan.bind(this)
+    this.sliderChangeStart = this.sliderChangeStart.bind(this)
+    this.sliderChange = this.sliderChange.bind(this)
+    this.sliderChangeComplete = this.sliderChangeComplete.bind(this)
+  }
+
   updateLoan(event) {
-    this.props.updateLoan(event.target.value)
+    this.props.updateLoanValue(event.target.value)
+    this.props.requestCalculation()
   }
   sliderChangeStart(event) {
     this.props.cancelRequest()
   }
-  sliderChange(event){
-    this.props.updateLoan(event)
+  sliderChange(value){
+    this.props.updateLoanValue(value)
   }
   sliderChangeComplete(event) {
-    this.props.requestCalculation(this.props.value, this.props.time, this.props.insurance)
+    this.props.requestCalculation()
   }
   render() {
     return (
@@ -38,10 +47,10 @@ class Borrow extends Component{
               orientation="horizontal"
               labels={{[this.props.min]:`${this.props.min}Kč`, [this.props.max]:`${this.props.max}Kč`}} 
               step={100}
-              onChangeStart={this.sliderChangeStart.bind(this)}
-              onChange={this.sliderChange.bind(this)}
+              onChangeStart={this.sliderChangeStart}
+              onChange={this.sliderChange}
               tooltip={false}
-              onChangeComplete={this.sliderChangeComplete.bind(this)}
+              onChangeComplete={this.sliderChangeComplete}
               style={{fontSize: '12px'}}
             />
           </div>
@@ -50,7 +59,7 @@ class Borrow extends Component{
               className="loanAmount form-control center"
               type="text"
               rows="5"
-              onChange={this.updateLoan.bind(this)}
+              onChange={this.updateLoan}
               value={this.props.value}/>
           </div>
           <div className="col-sm-1 text-left">Kč</div>
@@ -63,21 +72,15 @@ const mapStateToProps = state => {
   return {
     value: state.borrow.value,
     min: state.borrow.min,
-    max: state.borrow.max,
-    time: state.months.value,
-    insurance: state.insurance.with
+    max: state.borrow.max
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    updateLoan: newValue => {
-      dispatch(updateLoanValue(newValue))
-    },
-    requestCalculation: (value, time, insurance) => 
-      dispatch(requestLoanCalcDebounce(value, time, insurance)),
-    cancelRequest: () => 
-      dispatch(cancelRequest())
+    updateLoanValue: newValue => dispatch(updateLoanValue(newValue)),
+    requestCalculation: () => dispatch(requestLoanCalcDebounce()),
+    cancelRequest: () => dispatch(cancelRequest())
   }
 }
 
--- src/store/epics/calcLoanDebounce.js
+++ src/store/epics/calcLoanDebounce.js
@@ -6,19 +6,19 @@ import { CALC_LOAN_SEND_DEBOUNCE } from '../actions'
 import { QUERY_CANCELLED } from '../actions'
 import { server } from '../../config'
 
-const { map, switchMap, debounceTime, takeUntil } = require('rxjs/operators');
+const { map, switchMap, debounceTime, takeUntil, withLatestFrom } = require('rxjs/operators');
 
 const url = `http://${server.host}:${server.port}`
 
-export const requestLoanCalcDebounce = (amount, time, insurance) => 
-  ({ type: CALC_LOAN_SEND_DEBOUNCE, payload: { amount, time, insurance } })
+export const requestLoanCalcDebounce = () => ({ type: CALC_LOAN_SEND_DEBOUNCE })
 export const cancelRequest = () => ({ type: QUERY_CANCELLED, payload: { } })
 
-const calculateLoanDebounceEpic = action$  => action$.pipe(
+const calculateLoanDebounceEpic = (action$, state$)  => action$.pipe(
   ofType(CALC_LOAN_SEND_DEBOUNCE),
   debounceTime(1000),
-  switchMap(({ payload }) => 
-    ajax.getJSON(`${url}/loan?amount=${payload.amount}&time=${payload.time}&insurance=${payload.insurance}`).pipe(
+  withLatestFrom(state$),
+  switchMap(([, state]) => 
+    ajax.getJSON(`${url}/loan?amount=${state.borrow.value}&time=${state.months.value}&insurance=${state.insurance.with}`).pipe(
       map(response => updateSummaries(response)),
       takeUntil(action$.ofType(QUERY_CANCELLED))
     )
