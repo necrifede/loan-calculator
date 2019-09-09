62027ee3ebd82bba21e00efef753ecc4d900ce2d
Nestor Felipe <necrifede@gmail.com>
2019-09-09 07:45:01 +0200

    #20 Display friendly amounts

--- src/components/monthly-pay.js
+++ src/components/monthly-pay.js
@@ -11,7 +11,7 @@ const MonthlyPay = ({ monthly, requestLoanCalc }) => {
 	return (
 		<div className="container">
 			<p className="container">Monthly Pay</p>
-			<h1 className="container">{`${Math.trunc(monthly)} Kč`}</h1>
+			<h1 className="container">{`${Number(Math.trunc(monthly)).toLocaleString()} Kč`}</h1>
 			<Button className="row" type="submit" onClick={() => requestLoanCalc(10000, 24, 0.6)}>
 				Continue
 			</Button>
